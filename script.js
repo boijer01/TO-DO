6252913c3e11b80bb4999bcf95a0ea6bcf5252ec
boijersi <boijersi@arcada.fi>
2023-03-30 12:21:51 +0300

    Printar i korrekt prio och dag

--- script.js
+++ script.js
@@ -2,25 +2,30 @@ const toDoList = document.getElementById("toDoList");
 const printToDo = document.getElementById("printToDo");
 const prioSelect = document.getElementById("prioSel");
 const daySelect = document.getElementById("daySel");
+const today = new Date(); // Skapar ett Date-objekt
+const date = today.getUTCDay(); // Hämtar dagens datum
 
+
+// Kollar om toDo finns i localStorage, om inte så skapas en tom array
+// Annars så visas arrayn på skärmen
 if (localStorage.getItem("toDo") === null) {
     console.log("Den var null!");
     initializeEmptyToDoArray();
 } else {
     console.log("Den var inte null!");
-    displayToDoItems();
+    sortArray();
 }
 
 // Eventlistener för att ändra prioSelectValue när prioSelect ändras
-prioSelect.addEventListener("change", function() {
-let prioSelectValue = prioSelect.value;
-console.log("prio: "+ prioSelectValue);
+prioSelect.addEventListener("change", function () {
+    let prioSelectValue = prioSelect.value;
+    console.log("prio: " + prioSelectValue);
 });
 
 // Eventlistener för att ändra daySelectValue när daySelect ändras
-daySelect.addEventListener("change", function() {
-let daySelectValue = daySelect.value;
-console.log("day: " + daySelectValue);
+daySelect.addEventListener("change", function () {
+    let daySelectValue = daySelect.value;
+    console.log("day: " + daySelectValue);
 });
 
 
@@ -33,18 +38,16 @@ function initializeEmptyToDoArray() {
 
 // Funktion för att lägga till en todo
 function addItem() {
-    const today = new Date();
-    const date = today.getUTCDay(); // Hämtar dagens datum
     let textInput = document.getElementById("input");
     let textInputValue = textInput.value;
     const newTodo = {
         text: textInputValue,
-        day: date,
+        day: daySelect.value,
         priority: prioSelect.value
     };
     let newToDoJSON = JSON.stringify(newTodo);
     saveToDoItem(newToDoJSON);
-    
+
 }
 
 // Funktion för att hämta toDoArray från localStorage
@@ -55,14 +58,6 @@ function getToDoArray() {
     return toDoArray; // Returnerar toDoArray
 }
 
-// Funktion för att visa alla todos i arrayn under page load
-function displayToDoItems() {
-    let toDoArray = getToDoArray(); // Sparar returvärde från getToDoArray() i variabeln toDoArray
-    for (let i = 0; i < toDoArray.length; i++) {
-        let parseToDo = JSON.parse(toDoArray[i]);
-        printToDo.innerHTML += `<li>${parseToDo.text}</li>`;
-    }
-}
 
 // Funktion för att spara todo i arrayn
 function saveToDoItem(textInputValue) {
@@ -71,7 +66,7 @@ function saveToDoItem(textInputValue) {
     let toDoArrayStr = JSON.stringify(toDoArray); // Gör om toDoArray till en sträng
     localStorage.setItem("toDo", toDoArrayStr); // Sparar toDoArrayStr i localStorage
     let parseToDo = JSON.parse(toDoArray[toDoArray.length - 1]);
-    printToDo.innerHTML += `<li>${parseToDo.text}</li>`;
+    sortArray();
 }
 
 // Funktion för att ta bort todo från arrayn
@@ -80,6 +75,23 @@ function removeItem() {
     location.reload();
 }
 
+/*  Funktion för att sortera todos i arrayn
+    Prioritet 1 hamnar högst upp, prioritet 2 i mitten och prioritet 3 längst ner
+*/
 function sortArray() {
-   
+    printToDo.innerHTML = "";
+    let today = date;
+    console.log("Dagens datum: " + today);
+
+    let toDoArray = getToDoArray(); // Sparar returvärde från getToDoArray() i variabeln toDoArray
+    for (let n = 1; n < 4; n++) {
+        printToDo.innerHTML += "PRIORITET: " + n + "<br> ";
+        for (let i = 0; i < toDoArray.length; i++) {
+            let parseToDo = JSON.parse(toDoArray[i]);
+            if (parseToDo.priority == n && parseToDo.day == today) {
+                printToDo.innerHTML += `<li>${parseToDo.text}</li>`;
+            }
+        }
+
+    }
 }
